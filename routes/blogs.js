44df85b6b09b351408c3a553689ba33e4ba68be4
GitGagan123 <musidivalasagagan@gmail.com>
2024-12-08 21:44:29 +0530

    :tada: Implemented API for trending blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -30,6 +30,30 @@ const fetchBlogCategories = async () => {
   }
 };
 
+const fetchTrendingBlogs = async () => {
+  try {
+    const mongoClient = await mongoHelper.getConnectedMongoClient();
+    const trendingBlogs = await mongoClient
+      .db(config.dbName)
+      .collection(config.famousBlogsCollectionName)
+      .find({});
+
+    const trendingBlogsResults = await trendingBlogs.toArray();
+
+    const mappedTrendingBlogs = trendingBlogsResults.map((blog) => {
+      return {
+        id: blog._id,
+        picture: blog.picture,
+        author: blog.author,
+        quote: blog.quote,
+      };
+    });
+    return mappedTrendingBlogs;
+  } catch (err) {
+    console.log("Unable to Fetch Trending Blogs : ", err);
+  }
+};
+
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to Blogs API, explore other endpoints !!" });
 });
@@ -42,6 +66,17 @@ router.get("/categories", async (req, res) => {
     res.send({
       message:
         "No Blog Categories are present yet, please try again after sometime",
+    });
+  }
+});
+
+router.get("/trending", async (req, res) => {
+  const trendingBlogs = await fetchTrendingBlogs();
+  if (res.statusCode === 200 && trendingBlogs && trendingBlogs.length > 0) {
+    res.send(trendingBlogs);
+  } else {
+    res.send({
+      message: "No Trending Blogs at the moment , please try again later",
     });
   }
 });
